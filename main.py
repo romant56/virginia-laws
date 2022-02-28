de9ca9364aad07e1fd00ca1e21aff292b83e2625
romant56 <99830966+romant56@users.noreply.github.com>
2022-02-28 11:40:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ page = "https://law.lis.virginia.gov/api/CoVTitlesGetListOfJson"
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
 req = urllib.request.Request(url=page, headers=headers)
-filein = urllib.request.urlopen(req)
+try:
+    filein = urllib.request.urlopen(req)
 
 # create local list of dictionaries
 for item in filein:
